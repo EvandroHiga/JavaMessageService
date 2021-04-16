2dabb2eef163e8fcf1010346e686fff0cb3c5219
Higa <evandro_higa@hotmail.com>
2021-04-16 18:21:51 -0300

    impl do MessageListener

--- src/br/com/jms/consumer/Consumer.java
+++ src/br/com/jms/consumer/Consumer.java
@@ -1,11 +1,16 @@
 package br.com.jms.consumer;
 
+import java.util.Scanner;
+
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
 import javax.jms.Destination;
+import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
 import javax.jms.Session;
+import javax.jms.TextMessage;
 import javax.naming.InitialContext;
 
 public class Consumer {
@@ -26,8 +31,23 @@ public class Consumer {
 		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
 		MessageConsumer msgConsumer = session.createConsumer(fila);
 		
-		Message message = msgConsumer.receive();
-		System.out.println("### MSG RECEBIDA: " + message + "###");
+		msgConsumer.setMessageListener(new MessageListener() {
+			@Override
+			public void onMessage(Message message) {
+				TextMessage textMessage = (TextMessage) message;
+				try {
+					System.out.println("[ " + textMessage.getText() + " ]");
+					System.out.println("### pressione qualquer tecla para encerrar ###");
+				} catch (JMSException e) {
+					e.printStackTrace();
+				}
+							
+			}
+			
+		});
+		
+		System.out.println("### message listener funcionando ###");
+		new Scanner(System.in).nextLine();
 		
 		connection.close();
 		initialContext.close();
